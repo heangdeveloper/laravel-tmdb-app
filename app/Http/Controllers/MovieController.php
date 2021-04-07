67357a1d70580ec6bffcad505705271a4ba02774
Kimheang-Developer <simkimheang4@gmail.com>
2021-04-07 21:48:59 +0700

    Part 3

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $popularMovie = Http::withToken('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjQwYjRiM2Q2YjdhOGMyMGE5YWZiMTQ3YTk0OTNhOCIsInN1YiI6IjYwNjE4YWJmZTBjYTdmMDAzZTlhNjA4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Cl7A9OOaw2_yAzz_0YhBAQuFtFTzDvr6a7AoVzejLSw')
+        ->get('https://api.themoviedb.org/3/movie/popular')
+        ->json()['results'];
+
+        $upcomingMovie = Http::withToken('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjQwYjRiM2Q2YjdhOGMyMGE5YWZiMTQ3YTk0OTNhOCIsInN1YiI6IjYwNjE4YWJmZTBjYTdmMDAzZTlhNjA4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Cl7A9OOaw2_yAzz_0YhBAQuFtFTzDvr6a7AoVzejLSw')
+        ->get('https://api.themoviedb.org/3/movie/upcoming')
+        ->json()['results'];
+
+        return view('movie.index', [
+            'popularMovie' => $popularMovie,
+            'upcomingMovie' => $upcomingMovie
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/movie/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movie">
+            <h2 class="capitalize text-white text-lg font-semibold">Popular Movie</h2>
+            <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-8 gap-3">
+                @foreach ($popularMovie as $movie)
+                    @if ($loop->index < 16)
+                        <div class="mt-8 relative">
+                            <a href="#">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'] }}" class="hover:opacity-50 transition ease-in-out duration-150 rounded-lg"/>
+                            </a>
+                            <span class="ml-3 mt-3 border-2 border-yellow-500 rounded-full w-8 h-8 text-center absolute top-0 left-0 text-white font-semibold text-sm flex justify-center items-center">
+                                {{ $movie['vote_average'] * 10 }} <small class="text-xs">%</small>
+                            </span>
+                            <div class="mt-2">
+                                <a href="#" class="text-md pt-4 text-white font-semibold hover:text-yellow-500">{{ $movie['title'] }}</a>
+                                <div class="flex items-center text-gray-400 text-sm">
+                                    <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, y') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>                  
+        </div>
+
+        <div class="popular-movie pb-10">
+            <h2 class="capitalize text-white text-lg font-semibold">Upcoming Movie</h2>
+            <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-8 gap-3">
+                @foreach ($upcomingMovie as $movie)
+                    @if ($loop->index < 16)
+                        <div class="mt-8 relative">
+                            <a href="#">
+                                <img src="{{ 'https://image.tmdb.org/t/p/w500/' . $movie['poster_path'] }}" class="hover:opacity-50 transition ease-in-out duration-150 rounded-lg"/>
+                            </a>
+                            <span class="ml-3 mt-3 border-2 border-yellow-500 rounded-full w-8 h-8 text-center absolute top-0 left-0 text-white font-semibold text-sm flex justify-center items-center">
+                                {{ $movie['vote_average'] * 10 }} <small class="text-xs">%</small>
+                            </span>
+                            <div class="mt-2">
+                                <a href="#" class="text-md pt-4 text-white font-semibold hover:text-yellow-500">{{ $movie['title'] }}</a>
+                                <div class="flex items-center text-gray-400 text-sm">
+                                    <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, y') }}</span>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>                  
+        </div>
+
+    </div>
+@endsection
